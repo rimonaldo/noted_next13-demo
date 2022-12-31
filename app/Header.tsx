68433a2eb5305d8edb76736027db485184f2a5d3
Rimon <rimons2804@gmail.com>
2022-12-31 20:14:03 +0200

    added homepage title

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 function Header() {
    return (
       <header className="p-5 bg-blue-500 ">
+         <title>Noted</title>
          <Link className='px-2 py-1 bg-white text-blue-500 rounded-lg' href="/">Home</Link>
       </header>
    )
